dbe4579e9ce6fb86fd932f3fe5999bd9861c6a9f
NATHALIA CAPELA <nathaliacapela89@gmail.com>
2023-08-08 11:38:44 -0300

    teste login incorreto

--- cypress/e2e/Login/login.cy.js
+++ cypress/e2e/Login/login.cy.js
@@ -10,9 +10,15 @@ describe('Testes Funcional de Login', () => {
         cy.get('.title').should('contain','Products')
     });
 
-    
+    it('Validando Login incorreto  ', () => {
+        cy.visit("https://www.saucedemo.com/")
+        cy.get('[data-test="username"]').type("standard_id")
+        cy.get('[data-test="password"]').type("secret_sauce")
+        cy.get('[data-test="login-button"]').click()
+        cy.get('[data-test="error"]'). should('contain','Epic sadface: Username and password do not match any user in this service')
        
 });
 
+});
 
 
