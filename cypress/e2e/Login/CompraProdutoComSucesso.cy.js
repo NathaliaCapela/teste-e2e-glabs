e8cfd48ec97c28bf861b53a09db542970cd2441f
NATHALIA CAPELA <nathaliacapela89@gmail.com>
2023-08-10 16:29:33 -0300

    Checkout

--- cypress/e2e/Login/CompraProdutoComSucesso.cy.js
+++ cypress/e2e/Login/CompraProdutoComSucesso.cy.js
@@ -31,11 +31,22 @@ it('Fluxo da compra de produtos', () => {
     cy.get('[data-test="back-to-products"]').click()
 
     cy.get('#item_1_title_link > .inventory_item_name').click()
-    cy.get('[data-test="add-to-cart-sauce-labs-onesie"]').click()
+    cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
     cy.get('[data-test="back-to-products"]').click()
+    
 
+// Checagem da quantidade de produtos adicionados ao carrinho:
+cy.get('.shopping_cart_link').should('have.text','3')
+//Check no carrinho:
+cy.get('.shopping_cart_link').click()
+cy.get('.cart_list > :nth-child(3)').should('contain','Sauce Labs Onesie')
+cy.get('.cart_list > :nth-child(4)').should('contain','Sauce Labs Bike Light')
+cy.get('.cart_list > :nth-child(5)').should('contain','Sauce Labs Bolt T-Shirt')
 
-
-
-});
+//Checkout:
+cy.get('[data-test="checkout"]').click()
+cy.get('[data-test="firstName"]').type('Teste Primeiro Nome')
+cy.get('[data-test="lastName"]').type('Teste Ultimo Nome')
+cy.get('[data-test="postalCode"]').type('11035-260')
+cy.get('[data-test="continue"]').click()
 
